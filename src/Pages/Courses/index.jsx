9e3f753efa799cc9b8e48e055de3b55db1fb4869
Daniel <dny.velasquez@gmail.com>
2023-11-15 14:28:57 -0500

    margin main courses

--- src/Pages/Courses/index.jsx
+++ src/Pages/Courses/index.jsx
@@ -13,7 +13,7 @@ const Courses = () => {
         <Layout>
             <main className="bg-slate-400 min-h-screen w-full flex flex-col items-center">
                 <h1 className="mt-4 font-bold text-lg">COURSES</h1>
-                <div className="flex flex-wrap justify-center my-4 gap-4">
+                <div className="flex flex-wrap justify-center m-4 gap-4">
                     <DeepHtmlCssCourses />
                     <DeepJavascriptCourses />
                 </div>
