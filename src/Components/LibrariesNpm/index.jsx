a5e1720ab09102140a74accbb3f6c9f6ca09df71
Daniel <dny.velasquez@gmail.com>
2023-11-15 15:11:42 -0500

    NPM courses

---
+++ src/Components/LibrariesNpm/index.jsx
@@ -0,0 +1,209 @@
+import { useContext } from "react";
+import { Context } from "../../Context";
+
+const LibrariesNpm = () => {
+  const context = useContext(Context);
+
+  return (
+    <div className="border-2 p-2 rounded-lg h-96">
+      <h2
+        className="font-bold"
+        onClick={() => {
+          // context.openModalDiploma();
+          // context.setDiplomaName("Librerías y dependencias de Javascript con NPM - Javascript libraries and dependencies with NPM");
+          // context.setDiplomaImage("librerias-dependencias-javascrip-npm.png");
+        }}
+      >
+        Platzi - Librerías y dependencias de Javascript con NPM
+      </h2>
+      <h2 className="font-semibold">- Platzi - Javascript libraries and dependencies with NPM -</h2>
+      <div className="h-3/4 overflow-y-auto">
+        <ul className="mt-2 ml-2">
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "Frameworks y arquitecturas frontend - Frontend frameworks and architectures"
+                );
+                context.setDiplomaImage("frameworks-arquitecturas-frontend.png");
+              }}
+            >
+              Frameworks y arquitecturas frontend *
+            </p>
+            <p>- Frontend frameworks and architectures -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "Manipulación del DOM - DOM handling"
+                );
+                context.setDiplomaImage("manipulacion-dom.png");
+              }}
+            >
+              Manipulación del DOM *
+            </p>
+            <p>- DOM handling -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "Introducción empaquetadores web - Web packers introduction"
+                );
+                context.setDiplomaImage("introduccion-empaquetadores-web.png");
+              }}
+            >
+              Introducción empaquetadores web *
+            </p>
+            <p>- Web packers introduction -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "Frameworks y librerías de Javascript - Javascript frameworks and libraries"
+                );
+                context.setDiplomaImage("frameworks-librerías-javascript.png");
+              }}
+            >
+              Frameworks y librerías de Javascript *
+            </p>
+            <p>- Javascript frameworks and libraries -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "NPM"
+                );
+                context.setDiplomaImage("npm.png");
+              }}
+            >
+              NPM *
+            </p>
+            <p>- NPM -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "Programación reactiva con RXJS - Reactive programming with RXJS"
+                );
+                context.setDiplomaImage("programacion-reactiva-rxjs.png");
+              }}
+            >
+              Programación reactiva con RXJS *
+            </p>
+            <p>- Reactive programming with RXJS -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "SolidJS"
+                );
+                context.setDiplomaImage("solidjs.png");
+              }}
+            >
+              SolidJS *
+            </p>
+            <p>- SolidJS -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "SPA con Vanilla Javascript - SPA with Vanilla Javascript"
+                );
+                context.setDiplomaImage("spa-vanilla-javascript.png");
+              }}
+            >
+              SPA con Vanilla Javascript *
+            </p>
+            <p>- SPA with Vanilla Javascript -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "Svelte"
+                );
+                context.setDiplomaImage("svelte.png");
+              }}
+            >
+              Svelte *
+            </p>
+            <p>- Svelte -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "Vite.JS"
+                );
+                context.setDiplomaImage("vitejs.png");
+              }}
+            >
+              Vite.JS *
+            </p>
+            <p>- Vite.JS -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "Webpack"
+                );
+                context.setDiplomaImage("webpack.png");
+              }}
+            >
+              Webpack *
+            </p>
+            <p>- Webpack -</p>
+          </li>
+          <li>
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                context.openModalDiploma();
+                context.setDiplomaName(
+                  "Optimización web - Web performance"
+                );
+                context.setDiplomaImage("optimizacion-web.png");
+              }}
+            >
+              Optimización web *
+            </p>
+            <p>- Web performance -</p>
+          </li>
+        </ul>
+      </div>
+      <p className="mt-4 font-bold text-xs">* Click to see the diploma</p>
+    </div>
+  );
+};
+
+export default LibrariesNpm;
--- src/Pages/Courses/index.jsx
+++ src/Pages/Courses/index.jsx
@@ -2,8 +2,9 @@ import { useContext } from "react";
 import { Context } from "../../Context";
 import Layout from "../../Components/Layout";
 import ModalDiploma from "../../Components/ModalDiploma";
-import DeepHtmlCssCourses from "../../Components/DeepHtmlCssCourses"
-import DeepJavascriptCourses from "../../Components/DeepJavascriptCourses"
+import DeepHtmlCssCourses from "../../Components/DeepHtmlCssCourses";
+import DeepJavascriptCourses from "../../Components/DeepJavascriptCourses";
+import LibrariesNpm from "../../Components/LibrariesNpm";
 
 const Courses = () => {
 
@@ -12,10 +13,11 @@ const Courses = () => {
     return (
         <Layout>
             <main className="bg-slate-400 min-h-screen w-full flex flex-col items-center">
-                <h1 className="mt-4 font-bold text-lg">COURSES</h1>
+                <h1 className="mt-4 font-bold text-lg">WEB DEVELOPMENT COURSES</h1>
                 <div className="flex flex-wrap justify-center m-4 gap-4">
                     <DeepHtmlCssCourses />
                     <DeepJavascriptCourses />
+                    <LibrariesNpm />
                 </div>
                 {context.isModalDiplomaOpen ? <ModalDiploma /> : false}
             </main>
